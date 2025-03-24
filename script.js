5138a42195f0d8356d02f830c7e33e90bc0772f2
Sudhakar <ransangroups2025@gmail.com>
2025-03-24 20:05:05 +0530

    add landing page

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example: Scroll to section on button click
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
