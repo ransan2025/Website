87e0add47cc7165e9394143d8fb5770fd836a6aa
Sudhakar <ransangroups2025@gmail.com>
2025-05-04 20:58:49 +0530

    Latest site

--- script.js
+++
@@ -1,9 +0,0 @@
-// Example: Scroll to section on button click
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
-  });
-});
