cfc9fbdd90a2df40e44596da00433f844e5a708a
ransan2025 <ransangroups2025@gmail.com>
2025-08-01 12:27:35 +0530

    changes in database

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 // config.js
 
 // STEP 1: Choose environment
-const ENV = "test"; // Change to "test" when needed
+const ENV = "live"; // Change to "test" when needed
 
 // STEP 2: Define URLs per environment
 const CONFIG = {
